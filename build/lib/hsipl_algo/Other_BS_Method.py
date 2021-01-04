f56171d265a2a0d46de4e896f5245f29d7cc00e5
luckywilliam111 <70758442+luckywilliam111@users.noreply.github.com>
2021-01-05 01:51:21 +0800

    update

--- build/lib/hsipl_algo/Other_BS_Method.py
+++ build/lib/hsipl_algo/Other_BS_Method.py
@@ -7,18 +7,6 @@ Created on Sun Sep  6 17:14:38 2020
 
 import numpy as np
 
-def PCA_BS(im, num_band):
-    im = (im.reshape(im.shape[2], im.shape[0] * im.shape[1])).transpose()
-    
-    cov_mat = np.cov(im.T)
-    eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-    
-    sort_band = np.argsort(eigen_vals)
-    
-    band_select = sort_band[:num_band]
-    
-    return band_select
-
 def Uniform_BS(band_num, num):
     score = np.random.uniform(0, 1, [band_num, 1])
     weight = np.abs(score)
